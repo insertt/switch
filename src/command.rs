634db29813a3857c178937fd0822688593b8620f
insertt <inserttdev@gmail.com>
2019-08-15 05:45:57 +0200

    Initial version of Windows support

--- src/command.rs
+++ src/command.rs
@@ -1,8 +1,22 @@
 use std::io::{ stdin };
+#[cfg(target_os = "linux")]
 use std::fs;
 use std::process::{ exit };
 use crate::switch::{ SwitchRegistry, SwitchCategory };
+#[cfg(target_os = "linux")]
 use ttyecho::{ ttyecho };
+#[cfg(target_os = "windows")]
+use winapi::{
+    um::{
+        winreg::{ RegOpenKeyExA, RegSetValueExA, RegCloseKey, HKEY_CURRENT_USER, LSTATUS },
+        winuser:: { SendMessageTimeoutA, HWND_BROADCAST, WM_SETTINGCHANGE, SMTO_ABORTIFHUNG },
+        winnt::{ KEY_ALL_ACCESS, REG_SZ }
+    },
+    shared::{
+        minwindef::{ HKEY },
+        winerror::{ ERROR_SUCCESS }
+    }
+};
 
 pub fn set(registry: &mut SwitchRegistry, category_name: String, name: String, value: String) {
     let category = registry.get_category(&category_name);
@@ -72,25 +86,61 @@ pub fn apply(registry: &mut SwitchRegistry, mut category_name: String , mut name
 
     let category = registry.get_category(&category_name);
 
-    match category {
-        Some(category) => {
-            let pts = match fs::canonicalize("/proc/self/fd/0") {
-                Ok(pts) => pts,
-                Err(why) => panic!("Could not retrieve pty: {:#?}", why)
-            };
+        #[cfg(target_os = "windows")]
+    {
+        match category {
+            Some(category) => {
+                match category.get_variable(&name) {
+                    Some(env) => {
+                        unsafe {
+                            let mut hkey: HKEY = std::ptr::null_mut();
+                            let env_var_reg_key = "Environment";
+                            let status: LSTATUS = RegOpenKeyExA(HKEY_CURRENT_USER, env_var_reg_key.as_ptr() as *const i8, 0, KEY_ALL_ACCESS, &mut hkey);
 
-            match category.get_variable(&name) {
-                Some(env) => {
-                    ttyecho(pts.to_str().unwrap().to_string(), format!("export {} && clear", env.value), true);
-                    println!("Environment variables from category '{}' applied!", category_name);
+                            if status == ERROR_SUCCESS as i32 {
+                                let status = RegSetValueExA(hkey, env.key.as_ptr() as *const i8, 0, REG_SZ, env.value.as_ptr(), env.value.len() as u32);
+                                RegCloseKey(hkey);
+
+                                if status == ERROR_SUCCESS as i32 {
+                                    let status = SendMessageTimeoutA(HWND_BROADCAST, WM_SETTINGCHANGE, 0, "Environment".as_ptr() as isize, SMTO_ABORTIFHUNG, 1000, std::ptr::null_mut());
+                                    println!("Environment variable set!");
+                                }
+                            }
+                        }
+                    }
+                    _ => {
+                        eprintln!("Could not find environment variable with name '{}' in category '{}'", &name, &category_name);
+                    }
                 }
-                _ => {
-                    eprintln!("Could not find environment variable with name '{}' in category '{}'", &name, &category_name);
-                }
-            };   
-        },
-        None => {
-            eprintln!("Given category name is not registered!");
+            }
+            _ => {
+                eprintln!("Given category name is not registered!");
+            }
+        }
+        
+    }
+    #[cfg(target_os = "linux")]
+    {
+        match category {
+            Some(category) => {
+                let pts = match fs::canonicalize("/proc/self/fd/0") {
+                    Ok(pts) => pts,
+                    Err(why) => panic!("Could not retrieve pty: {:#?}", why)
+                };
+
+                match category.get_variable(&name) {
+                    Some(env) => {
+                        ttyecho(pts.to_str().unwrap().to_string(), format!("export {} && clear", env.value), true);
+                        println!("Environment variables from category '{}' applied!", category_name);
+                    }
+                    _ => {
+                        eprintln!("Could not find environment variable with name '{}' in category '{}'", &name, &category_name);
+                    }
+                };   
+            },
+            None => {
+                eprintln!("Given category name is not registered!");
+            }
         }
     }
 }
